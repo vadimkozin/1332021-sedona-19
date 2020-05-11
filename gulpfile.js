86147604beaf8614058f00674b0dfdc55d02b02a
Vadim Kozin <ulin.salma@yandex.ru>
2020-05-11 15:31:29 +0300

    автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,97 @@
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
+var server = require("browser-sync").create();
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
+var uglify = require('gulp-uglify');
+var pipeline = require('readable-stream').pipeline;
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+      "source/fonts/**/*.{woff,woff2}",
+      "source/img/**",
+      "source/js/**",
+      "source/*.ico"
+    ], {
+      base: "source"
+})
+    .pipe(gulp.dest("build"));
+});
+
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream()); //?
+});
+
+gulp.task("sprite", function () {
+  return gulp.src([
+    "source/img/icon-*.svg",
+    "source/img/logo-htmlacademy.svg"])
+    .pipe(svgstore({inlineSvg: true}))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml( [include()] ))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({quality: 75, progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("script-copy", function () {
+  return gulp.src("source/js/*.js")
+    .pipe(gulp.dest("build/js"));
+});
+
+gulp.task('script', function () {
+  return gulp.src(["source/js/*.js", "!source/js/*.min.js"])
+        .pipe(rename({suffix: ".min"}))
+        .pipe(uglify())
+        .pipe(gulp.dest("build/js"));
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -31,9 +101,18 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.scss", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
-  gulp.watch("source/**/*.js").on("change", server.reload);
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/**/*.js", gulp.series("script-copy", "script", "refresh"));
+
+  // gulp.watch("source/**/*.js").on("change", server.reload);
 
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("build", gulp.series("clean", "copy", "css", "script", "sprite", "html"));
+gulp.task("start", gulp.series("build", "server"));
--- source/js/app.js
+++ source/js/app.js
@@ -1,20 +1,107 @@
 function init() {
-  var navMain = document.querySelector('.main-nav');
-  var navToggle = document.querySelector('.main-nav__toggle');
+  setMainNavigation();
+  setTabindexOnForm();
+  setFocus();
 
-  navMain.classList.remove('main-nav--nojs');
+  // Главная навигация
+  function setMainNavigation() {
+    var navMain = document.querySelector('.main-nav');
+    var navToggle = document.querySelector('.main-nav__toggle');
 
-  navToggle.addEventListener('click', function() {
+    navMain.classList.remove('main-nav--nojs');
 
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
+    navToggle.addEventListener('click', function() {
 
-    } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
+      if (navMain.classList.contains('main-nav--closed')) {
+        navMain.classList.remove('main-nav--closed');
+        navMain.classList.add('main-nav--opened');
+
+      } else {
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+      }
+    }, false);
+  }
+
+
+  // Порядок перемещения по элементам на форме
+  function setTabindexOnForm() {
+
+    if (!document.querySelector('.form')) {
+      return;
     }
-  }, false);
+
+    // https://developer.mozilla.org/en-US/docs/Web/CSS/Media_Queries/Testing_media_queries
+    var mediaQueryList = window.matchMedia("(min-width: 768px)");
+    mediaQueryList.addListener(handleBreakpointChange);
+    handleBreakpointChange(mediaQueryList);
+
+    function handleBreakpointChange(evt) {
+      // элементы, учавствующие в перемещении по <tab>
+      var person = document.querySelectorAll('.person__item-input');
+      var impression = document.getElementsByName('impression')[0];
+      var phone = document.getElementsByName('phone')[0];
+      var email = document.getElementsByName('email')[0];
+      var visited = document.querySelectorAll('.visited__input');
+      var emotions = document.getElementsByName('emotions')[0];
+      var button = document.querySelector('.form__button');
+      var social = document.querySelectorAll('.social__link');
+      var copyright = document.querySelector('.copyright__link');
+      var index = 0;
+
+      if (evt.matches) {  // >= 768px
+        setOrder("reset");
+      } else {            // < 768px
+        setOrder("next")
+      }
+
+      // возвращает индекс
+      function getIndex(mode) {
+        return (mode === "next") ? index+=1 : 0;
+      }
+
+      // устанавливает порядок перехода по элементам
+      function setOrder(mode) {
+        person.forEach(function(el) { el.tabIndex = getIndex(mode) })
+        phone.tabIndex = getIndex(mode);
+        email.tabIndex = getIndex(mode);
+        impression.tabIndex = getIndex(mode);
+        visited.forEach(function(el) { el.tabIndex = getIndex(mode) })
+        emotions.tabIndex = getIndex(mode);
+        button.tabIndex = getIndex(mode);
+        social.forEach(function(el) { el.tabIndex = getIndex(mode) })
+        copyright.tabIndex = getIndex(mode);
+      }
+
+      // для отладки
+      // function printOrder() {
+      //   console.log('person:', person[0].tabIndex);
+      //   console.log('phone:', phone.tabIndex);
+      //   console.log('email:', email.tabIndex);
+      //   console.log('impression:', impression.tabIndex);
+      //   console.log('visited:', visited[0].tabIndex);
+      //   console.log('emotions:', emotions.tabIndex);
+      //   console.log('button:', button.tabIndex);
+      //   console.log('social:', social[0].tabIndex);
+      //   console.log('copyright:', copyright.tabIndex);
+      // }
+    }
+  }
+
+
+  // macOS: Firefox и Safari после выбора мышкой элементов формы (checkbox, radio, button) не показывают окантовку элемента
+  // приходиться явно устанавливает фокус при клике
+  // а сама окантовка стилизуется через CSS
+  function setFocus() {
+    var selectors = ['.social__item a', 'button', '.panel__btn', '.panel__slider', '.form input[type=checkbox]', '.form input[type=radio]'];
+    var elements = document.querySelectorAll(selectors.join());
+
+    elements.forEach(function(el) {
+      el.addEventListener('click', function(event) {
+        el.focus();
+      });
+    })
+  }
 }
-//
+
 window.addEventListener("load", init, false);
